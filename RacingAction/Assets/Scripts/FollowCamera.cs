07f46c93d37be0011800b2312d6dc02d7edd3f3c
白枝 <1701319@st.com>
2019-11-19 13:09:49 +0900

    テストでカメラのスクリプト追加

---
+++ RacingAction/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField, Tooltip("追従するターゲット")]
+    private GameObject _target = null;
+    [SerializeField, Tooltip("カメラの追従距離")]
+    private float _distance = 10;
+    [SerializeField, Tooltip("カメラの角度")]
+    private float _rotate = 0;
+    void Start()
+    {
+        
+    }
+
+    // ターゲットの追従
+    private void Follow()
+    {
+        transform.position = _target.transform.position + new Vector3(0, 4, -_distance);
+        transform.rotation = Quaternion.Euler(new Vector3(_rotate, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // カメラ処理は移動が完了した後に行いたいのでLateUpdate内で書く
+    private void LateUpdate()
+    {
+        Follow();
+    }
+}
