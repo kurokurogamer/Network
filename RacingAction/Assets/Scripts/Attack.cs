c87fdf966baa6ae65838a6d05d83f0f65c1f416f
aaaa <aaa>
2020-01-21 12:20:23 +0900

    テスト

---
+++ RacingAction/Assets/Scripts/Attack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject bullet;
+    //スピード
+    float speed;
+
+
+    void Start()
+    {
+        speed = 5;
+    }
+
+    public void Shot(GameObject car)
+    { 
+        //runcherbulletにbulletのインスタンスを格納
+        GameObject runcherBullet = GameObject.Instantiate(bullet);
+
+        //アタッチしているオブジェクトの前方にbullet speedの速さで発射
+        runcherBullet.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        runcherBullet.transform.position = car.transform.position;
+    }
+}
