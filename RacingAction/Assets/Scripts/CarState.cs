40ea5e4291bf8ac960d030c8c49b1cc5d2d3a7b2
白枝 <1701319@st.com>
2020-02-03 19:06:27 +0900

    UDPSYstemの追加

--- RacingAction/Assets/Scripts/CarState.cs
+++ RacingAction/Assets/Scripts/CarState.cs
@@ -17,6 +17,28 @@ public struct CarState
     // ゴール判定
     [DataMember(Name = "goalCar")]
     public bool goalCar;
+    public byte[] ToByte()
+    {
+        pos = new float[3];
+        for(int i = 0; i < pos.Length; i++)
+        {
+            pos[i] = 10.0f;
+        }
+        byte[] x = BitConverter.GetBytes(pos[0]);
+        byte[] y = BitConverter.GetBytes(pos[1]);
+        byte[] z = BitConverter.GetBytes(pos[2]);
+        return MargeByte(MargeByte(x, y), z);
+    }
+    public static byte[] MargeByte(byte[] baseByte, byte[] addByte)
+    {
+        byte[] b = new byte[baseByte.Length + addByte.Length];
+        for (int i = 0; i < b.Length; i++)
+        {
+            if (i < baseByte.Length) b[i] = baseByte[i];
+            else b[i] = addByte[i - baseByte.Length];
+        }
+        return b;
+    }
     //(名前仮)(車がゴールしているのか)
     //public CarState()
     //{
---
+++ RacingAction/Assets/Scripts/main.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main : MonoBehaviour
+{
+    public GameObject gameObject;　//動かすオブジェクトをUnity上でアタッチ
+    public Text text; //座標表示用
+
+    string ipAddr = "172.20.39.22"; // ホスト側IPアドレス
+    string ipAddr2 = "172.20.39.32"; //クライアント側IPアドレス
+
+    Vector3 vector3 = new Vector3(0, 0, 0);
+    UDPSystem udpSystem;
+    char device = 'A'; // ホスト側動作はA,クライアント側動作はB
+
+    private void Awake()
+    {
+        switch (device)
+        {
+            case 'A':
+                udpSystem = new UDPSystem(null);
+                udpSystem.Set(ipAddr, 5001, ipAddr2, 5002);
+                break;
+            case 'B':
+                udpSystem = new UDPSystem((x) => Receive(x));
+                udpSystem.Set(ipAddr2, 5002, ipAddr, 5001);
+                udpSystem.Receive();
+                break;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (device == 'A')
+        {
+            vector3 = gameObject.transform.position;
+            CarState sendData = new CarState();
+            udpSystem.Send(sendData.ToByte(), 99);
+        }
+        if (device == 'B')
+        {
+            gameObject.transform.position = vector3;
+        }
+        text.text = "(" + vector3.x + "," + vector3.y + "," + vector3.z + ")";
+    }
+
+    void Receive(byte[] bytes)
+    {
+        Debug.Log("Client");
+    }
+}
